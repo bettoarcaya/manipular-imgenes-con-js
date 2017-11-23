b78b64ed7333f417386f89fcabee7028e2bb08f1
b3tt0 <betoarcaya@gmail.com>
2017-11-23 11:45:13 -0400

    resolviendo el problema de obtencion de informacion de la imagen establecida en el canvas

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
 
-let canvas = document.getElementById("lienzo");
+document.getElementById("imagen").onload = function() {
+  var canvas = document.getElementById("lienzo");
+  var cntx = canvas.getContext("2d");
+  var img = document.getElementById("imagen");
+  cntx.drawImage(img, 0, 0);
+  var imgData = cntx.getImageData(0, 0, canvas.width, canvas.height);
+  console.log(imgData.data[0], imgData.data[1], imgData.data[2], imgData.data[3+4]);
+};
+
+
+
+
+/*let canvas = document.getElementById("lienzo");
 let cntx = canvas.getContext("2d");
 let name;
+let img = document.getElementById("imagen");
+img.onload = function(){ cntx.drawImage(img, 0, 0); }
+var imgData = cntx.getImageData(0, 0, canvas.width, canvas.height);
+console.log(imgData.data.length);*/
 
 /**cargamos la imagen al canvas */
-function cargarImg() {
+/*function cargarImg() {
   img = new Image();
   img.src = name;
   img.onload = function(){ cntx.drawImage(img, 0, 0); }
@@ -13,8 +29,7 @@ function cargarImg() {
 }
 
 /**obtenemos el nombre del fichero que se ingreso al input */
-function nombre(fic) {
+/*function nombre(fic) {
   fic = fic.split('\\');
   name = fic[fic.length-1];
-}
-
+}*/
